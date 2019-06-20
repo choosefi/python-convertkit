a6219fd0e0f1e823a41e6ae950f080737738ffdd
William McVey <wam@wamber.net>
2019-06-20 17:12:59 -0400

    Major rewrite of core API elements

--- convertkit/main.py
+++ convertkit/main.py
@@ -1,70 +1,172 @@
 import requests
 from unittest import TestCase
 
+import requests
+
+
 class APIError(Exception):
     pass
 
-class APIModel(object):
-    def __init__(self, creds, requester, params):
-        self.creds = creds
-        self.requester = requester
-        self.params = params
+
+class APIModel:
+    def __init__(self, json_blob, api):
+        self.log = logging.getLogger("ConvertKit." + self.__class__.__name__)
+        self.api = api
+        self.obj = self.decode(json_blob, api)
 
     def __getattr__(self, attr):
-        try:
-            return self.params[attr]
-        except:
-            raise AttributeError
+        return self.obj[attr]
+
+    @staticmethod
+    def decode(blob, api):
+        """A basic decoder that simply returns the blob that is passed in
+        """
+        return blob
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {" ".join([f"{k}={v!r}" for k,v in self.obj.items()])}>'
+
+class SubscriptionMixin:
+    """A Mixin for object types that support subscriptions/membership
+
+    Requires a class or instance variable MODEL_ENDPOINT
+    """
+
+    def list_subscriptions(self, sort_order="asc", subscriber_state=None):
+        if not self.api.api_secret:
+            raise APIError("Form subscription listing endpoint needs API secret")
+        factory = lambda response: [Subscription(x, api=self.api) for x in response['subscriptions']]
+        resp = self.api.GET(f'{self.MODEL_ENDPOINT}/{self.id}/subscriptions', factory=factory, api_secret=self.api.api_secret)
+        self.log.info(f"{self} subscriptions: {resp}")
+        return resp
+
+    def add_subscriber(self, email, first_name=None, params=None, **kwargs):
+        params = dict(params) if params else {}
+        params.update(kwargs)
+        if first_name:
+            params["first_name"] = first_name
+        resp = self.api.POST(f'{self.MODEL_ENDPOINT}/{self.id}/subscribe',
+                             factory=lambda x: Subscription(x['subscription'], api=self.api),
+                             email=email, params=params)
+        return resp
+
+
+class Form(APIModel, SubscriptionMixin):
+    MODEL_ENDPOINT = "/forms"
+
+    def __str__(self):
+        return f"{self.id} {self.name}{' '+self.title if 'title' in self.obj else ''}"
 
 class Subscriber(APIModel):
-    def __repr__(self):
-        return '<Subscriber {}>'.format(self.id)
+    pass
 
-class Form(APIModel):
-    def add_subscriber(self, email, first_name):
-        resp = self.requester.post(
-            '{}{}'.format(self.creds.base_url, 'v3/forms/{}/subscribe'.format(self.id)),
-            data={
-                'email': email,
-                'name': first_name,
-                }, params={'api_key': self.creds.api_key})
-        if resp.status_code >= 300:
-            raise APIError(resp.content)
 
-        return Subscriber(self.creds, self.requester, resp.json()['subscription'])
+class Subscription(APIModel):
+    @staticmethod
+    def decode(blob, api):
+        blob["subscriber"] = Subscriber(blob["subscriber"], api)
+        return blob
 
-    def __repr__(self):
-        return '<Form:{}>'.format(self.params.get('name', 'Unknown'))
 
-class CredentialsObject(object):
-    def __init__(self, api_key, base_url="https://api.convertkit.com/"):
-        self.api_key = api_key
-        self.base_url = base_url
+class Account(APIModel):
+    pass
 
-class Forms(object):
-    def __init__(self, creds, requester):
-        self.creds = creds
-        self.requester = requester
+class Course(APIModel, SubscriptionMixin):
+    MODEL_ENDPOINT = "/courses"
 
-    def list(self):
-        resp = self.requester.get(
-            '{}{}'.format(self.creds.base_url, 'v3/forms'),
-            params={
-                'api_key':self.creds.api_key
-                })
-        if resp.status_code >= 300:
-            raise APIError(resp.content)
-        return [Form(self.creds, self.requester, x) for x in resp.json()['forms']]
+
+class Tag(APIModel, SubscriptionMixin):
+    MODEL_ENDPOINT = "/tags"
+
+
 
 
 class ConvertKit(object):
-    def __init__(self, api_key, base_url="https://api.convertkit.com/", requester=None):
-        self.creds = CredentialsObject(api_key, base_url)
-        self.requester = requester or requests
+    BASE_URL = "https://api.convertkit.com/v3"
 
-    @property
-    def forms(self):
-        return Forms(self.creds, self.requester)
+    def __init__(self, api_key, api_secret=None, requester=None):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.requester = requester or requests
+        self.log = logging.getLogger(self.__class__.__name__)
+
+    def GET(self, endpoint, factory=None, params=None, **kwargs):
+        """Make a GET request to an API endpoint
+        """
+        params = dict(params) if params is not None else {}
+        params["api_key"]=self.api_key
+        params.update(kwargs)
+        resp = self.requester.get(
+            ''.join([self.BASE_URL, endpoint]),
+            params=params)
+        self.log.debug(f"Response: {resp}  status: {resp.status_code}   json: {resp.json()}")
+        if resp.status_code >= 300:
+            raise APIError(resp.content)
+        if factory:
+            return factory(resp.json())
+        else:
+            return resp.json()
+
+    def POST(self, endpoint, factory=None, params=None, **kwargs):
+        """Make a GET request to an API endpoint
+        """
+        params = dict(params) if params is not None else {}
+        params["api_key"]=self.api_key
+        params.update(kwargs)
+        resp = self.requester.post(
+            ''.join([self.BASE_URL, endpoint]),
+            data=params)
+        self.log.debug(f"Response: {resp}  status: {resp.status_code}")
+        if resp.status_code >= 300:
+            raise APIError(resp.content)
+        # import code; code.interact(banner=f"POST> {endpoint} {params}", local=dict(globals(), **locals()))
+        if factory:
+            return factory(resp.json())
+        else:
+            return resp.json()
+
+    def list_forms(self):
+        factory = lambda response: [Form(x, api=self) for x in response['forms']]
+        resp = self.GET("/forms", factory)
+        self.log.info(f"list_forms={resp}")
+        return resp
+
+    def find_form(self, form_id=None, form_name=None):
+        forms = self.list_forms()
+        self.log.info(f'find_form ids = {",".join([str(x.id) for x in forms])}')
+        if form_id is not None:
+           forms = [f for f in forms if f.id == form_id]
+        if form_name is not None:
+           forms = [f for f in forms if f.name == form_name]
+        if len(forms) == 0:
+            raise RuntimeError(f"Did not find a form with matching search form_id={form_id} form_name={form_name}")
+        if len(forms) > 1:
+            raise RuntimeError(f"More than one form matched search form_id={form_id} form_name={form_name}")
+        return forms.pop()
+
+    def account(self):
+        if not self.api_secret:
+            raise APIError("account endpoint needs API secret")
+        resp = self.GET("/account", lambda x: Account(**x), api_secret=self.api_secret)
+        self.log.info(f"account={resp}")
+        return resp
+
+    def sequences(self):
+        factory = lambda response: [Course(x, api=self) for x in response['courses']]
+        resp = self.GET("/courses", factory)
+        self.log.info(f"sequences={resp}")
+        return resp
+
+    def tags(self):
+        factory = lambda response: [Tag(x, api=self) for x in response['tags']]
+        resp = self.GET("/tags", factory)
+        self.log.info(f"tags={resp}")
+        return resp
+
+    def create_tag(self, name, description):
+        resp = self.POST("/tags", factory=lambda x: Tag(x, api=self), name=name, description=description)
+        self.log.info(f"create_tag={resp}")
+        return resp
 
 
 class FormTestCase(TestCase):
